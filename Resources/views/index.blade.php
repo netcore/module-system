a643ddfd56273d2fa2eb634550efd6e3af917c5f
speekadievs <arturs@54blue.com>
2018-02-15 14:05:31 +0200

    added base

---
+++ Resources/views/index.blade.php
@@ -0,0 +1,20 @@
+@extends('admin::layouts.master')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-inverse">
+                <div class="panel-heading">
+                    <h4 class="panel-title">System Information</h4>
+                </div>
+                <div class="panel-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+
+@endsection
